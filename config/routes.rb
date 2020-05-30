eaf1f5c47945bac899d315b8b6dc4653e954510e
jenaalsup <63277580+jenaalsup@users.noreply.github.com>
2020-05-30 00:10:31 -0700

    Add delete feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  resources :articles
+  # this is only necessary if not all routes are being used - resources :articles, only: [:show, :index, :new, :create, :edit, :update]
 end
