468e980a0140b69ba675020961c854bc6233d2c1
jenaalsup <63277580+jenaalsup@users.noreply.github.com>
2020-06-10 12:17:12 -0700

    Add user login form and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   # this is only necessary if not all routes are being used - resources :articles, only: [:show, :index, :new, :create, :edit, :update]
   get 'signup', to: 'users#new' 
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
