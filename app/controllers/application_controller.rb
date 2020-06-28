b33f6e37eca05c7fb4227ad00d3a09b8ea67127a
jenaalsup <63277580+jenaalsup@users.noreply.github.com>
2020-06-27 17:14:06 -0700

    Add authentication and controller helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :current_user, :logged_in? # makes these methods available to views as well, not just controllers
+  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] # if current user already exists, return current user, else, find the user in the database
+  end
+
+  def logged_in?
+    !!current_user # turn current user into a boolean by using !!
+  end
+
 end
