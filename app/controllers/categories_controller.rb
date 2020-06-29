a62a4f070fca114ef8aacac885109ec8ec28c848
jenaalsup <63277580+jenaalsup@users.noreply.github.com>
2020-06-28 20:20:44 -0700

    Only allow admin users to create categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,9 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
 
-  def new 
+  def new
     @category = Category.new
-  end 
+  end
 
   def create
     @category = Category.new(category_params)
@@ -14,18 +15,24 @@ class CategoriesController < ApplicationController
     end
   end
 
-  def index 
+  def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
-  end 
+  end
 
   def show
     @category = Category.find(params[:id])
-  end 
-
+  end
+  
   private
-
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = "Only admins can perform that action"
+      redirect_to categories_path
+    end
   end
 
 end
